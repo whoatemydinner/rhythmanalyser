e911f1e83969dd697b6acaf22170eded75d04fbc
Piotr <kierzekp@gmail.com>
2020-02-28 16:21:42 +0100

    Annotations in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 from gui import MainWindow
 
+# TODO: move filepath reference elsewhere
 filepath = None
 
+# create and display a main window object
 main_window = MainWindow.MainWindow(filepath)
 main_window.start()
